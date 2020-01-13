c547d90dd6ef4a034d207f16c1b985d2eaf6f51f
v8077971 <v8077971@TU45390>
2020-01-13 11:59:31 +0000

    Added Staff Bookings to the Event Db Context

---
+++ ThAmCo.Events/Data/Migrations/20200113115846_addStaffBookings.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThAmCo.Events.Data.Migrations
+{
+    public partial class addStaffBookings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StaffBooking",
+                schema: "thamco.events",
+                columns: table => new
+                {
+                    StaffId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StaffId1 = table.Column<int>(nullable: true),
+                    EventId = table.Column<int>(nullable: false),
+                    Attended = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StaffBooking", x => x.StaffId);
+                    table.ForeignKey(
+                        name: "FK_StaffBooking_Events_EventId",
+                        column: x => x.EventId,
+                        principalSchema: "thamco.events",
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StaffBooking_Staff_StaffId1",
+                        column: x => x.StaffId1,
+                        principalSchema: "thamco.events",
+                        principalTable: "Staff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffBooking_EventId",
+                schema: "thamco.events",
+                table: "StaffBooking",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffBooking_StaffId1",
+                schema: "thamco.events",
+                table: "StaffBooking",
+                column: "StaffId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StaffBooking",
+                schema: "thamco.events");
+        }
+    }
+}
