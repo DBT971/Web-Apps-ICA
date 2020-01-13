f431bec967ae4a8eec4edf2d916a4f17fa3eaf47
v8077971 <v8077971@TU45390>
2020-01-13 11:35:14 +0000

    Added Staff Table to Database

---
+++ ThAmCo.Events/Data/Migrations/20200113113318_addStaff.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThAmCo.Events.Data.Migrations
+{
+    public partial class addStaff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "Staff",
+                schema: "thamco.events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Surname = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    FirstAider = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staff", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Staff",
+                schema: "thamco.events");
+
+        }
+    }
+}
